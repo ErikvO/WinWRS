57bc12bd5e2fe39cc0415dcf3c431a449305544f
Erik <ErikvO@users.noreply.github.com>
2017-07-30 20:39:04 +0200

    Remove quotes from ShutdownParameters parameter

--- WinWRS/Commandline/ShutdownWindowsSMB.bat
+++ WinWRS/Commandline/ShutdownWindowsSMB.bat
@@ -2,6 +2,6 @@
 set Computer=%1
 set Username=%2
 set Password=%3
-set ShutdownParameters=%4
+set ShutdownParameters=%~4
 net use \\%Computer%\IPC$ %Password% /USER:%Username%
 shutdown %ShutdownParameters% /t 0 /m \\%Computer%
